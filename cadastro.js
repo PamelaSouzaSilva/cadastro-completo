5a1a7f2f67c4dbf3952c121b3b5b1260f8226bfb
Pamela <p.silva39@alunoescolassociais.org.br>
2024-10-10 08:04:41 -0300

    Valida Validação do Formulário de Dados Pessoais

--- cadastro.js
+++ cadastro.js
@@ -21,3 +21,24 @@ const validarCPF = (cpf) => {
     if (resto === 10 || resto === 11) resto = 0;
     return resto === parseInt(cpf.substring(10, 11));
 };
+
+// Mensagem de Erro
+const mostrarErro = (campo, mensagem) => {
+    alert(mensagem);
+    campo.focus();
+};
+
+// Validação do Formulário de Dados Pessoais
+const validarDadosPessoais = () => {
+    const email = document.getElementById('inputEmail4').value;
+    const cpf = document.getElementById('CPF').value;
+    if (!validarEmail(email)) {
+        mostrarErro(document.getElementById('inputEmail4'), 'Por favor, informe o E-mail!');
+        return false;
+    }
+    if (!validarCPF(cpf)) {
+        mostrarErro(document.getElementById('CPF'), 'CPF inválido!');
+        return false;
+    }
+    return true;
+};
